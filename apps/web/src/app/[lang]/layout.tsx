273cba0194ae83c9ef56b58bed3fa8987a91c837
menno-lab <65033632+menno-lab@users.noreply.github.com>
2023-06-27 10:46:23 +0300

    color mode fixes

--- apps/web/src/app/[lang]/layout.tsx
+++ apps/web/src/app/[lang]/layout.tsx
@@ -1,8 +1,8 @@
 import { dir } from 'i18next';
 import { languages } from '../../i18n/settings';
-import { ThemeProvider } from 'ui';
+import { ThemeProvider, Navbar, NavItem } from 'ui';
 import { RouteWithChildren } from '../../utils/types';
-import { Navbar } from '../../components/Navbar';
+import { useTranslation } from '../../i18n';
 
 export const metadata = {
     title: "Menno's CV Website",
@@ -14,11 +14,13 @@ export async function generateStaticParams() {
 }
 
 export default async function RootLayout({ children, params: { lang } }: RouteWithChildren) {
+    const { t } = await useTranslation(lang);
+    const navItems = t('navItems') as unknown as NavItem[];
     return (
         <html lang={lang} dir={dir(lang)}>
             <body>
                 <ThemeProvider>
-                    <Navbar lang={lang} />
+                    <Navbar navItems={navItems} />
                     {children}
                 </ThemeProvider>
             </body>
