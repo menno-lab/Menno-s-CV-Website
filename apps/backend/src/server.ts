6765b3157b1cb6ce6787bf1b0e369ea74c3ecf3c
menno-lab <65033632+menno-lab@users.noreply.github.com>
2023-07-01 21:20:57 +0300

    cleanup

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,27 +1,8 @@
-import { inferAsyncReturnType, initTRPC } from '@trpc/server';
 import { awsLambdaRequestHandler } from '@trpc/server/adapters/aws-lambda';
-import type { CreateAWSLambdaContextOptions } from '@trpc/server/adapters/aws-lambda';
-import type { APIGatewayProxyEvent } from 'aws-lambda';
 import { contactRoutes } from './routes/contactRoutes';
-
-function createContext({ event }: CreateAWSLambdaContextOptions<APIGatewayProxyEvent>) {
-    return {
-        event: event,
-        apiVersion: (event as { version?: string }).version ?? '1.0',
-        user: event.headers['x-user'],
-    };
-}
-type Context = inferAsyncReturnType<typeof createContext>;
-
-const t = initTRPC.context<Context>().create();
-
-const publicProcedure = t.procedure;
-const router = t.router;
+import { createContext, router } from './trpc';
 
 const appRouter = router({
-    greet: publicProcedure.query(() => {
-        return `Greetings, user`;
-    }),
     contact: contactRoutes,
 });
 
--- apps/backend/src/trpc.ts
+++ apps/backend/src/trpc.ts
@@ -1,6 +1,17 @@
-import { initTRPC } from '@trpc/server';
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import { CreateAWSLambdaContextOptions } from '@trpc/server/adapters/aws-lambda';
+import { APIGatewayProxyEvent } from 'aws-lambda';
 
-const t = initTRPC.context().create();
+export function createContext({ event }: CreateAWSLambdaContextOptions<APIGatewayProxyEvent>) {
+    return {
+        event: event,
+        apiVersion: (event as { version?: string }).version ?? '1.0',
+        user: event.headers['x-user'],
+    };
+}
+
+type Context = inferAsyncReturnType<typeof createContext>;
+const t = initTRPC.context<Context>().create();
 
 export const middleware = t.middleware;
 export const router = t.router;
