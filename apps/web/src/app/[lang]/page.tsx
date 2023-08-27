8f5c7e36d37b92cd7e8e2dca6625ad63bcb959a8
menno-lab <65033632+menno-lab@users.noreply.github.com>
2023-08-27 12:17:46 +0300

    social proof added

--- apps/web/src/app/[lang]/page.tsx
+++ apps/web/src/app/[lang]/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Hero } from './_home-sections/hero/Hero';
+import { Text } from 'ui';
+import { SocialProof } from './_home-sections/social-proof/SocialProof';
+import { useTranslation } from '@/i18n';
 
-export default function Home() {
+export default async function Home() {
+    const { t } = await useTranslation();
     return (
         <div>
             <Hero />
+            <Text as='h3' fontSize='3xl' fontWeight='bold' textAlign='center'>
+                {t('hero.socialProof')}
+            </Text>
+            <SocialProof />
         </div>
     );
 }
